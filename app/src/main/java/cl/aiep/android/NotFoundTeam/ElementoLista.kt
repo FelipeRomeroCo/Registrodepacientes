9946d790277fe918f9358877bbed9c9aaae21597
Felipe Romero <tmfromero@gmail.com>
2022-10-17 10:31:49 -0300

    Se agregan más vistas

---
+++ app/src/main/java/cl/aiep/android/NotFoundTeam/ElementoLista.kt
@@ -0,0 +1,11 @@
+package cl.aiep.android.NotFoundTeam
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ElementoLista : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_elemento_lista)
+    }
+}
